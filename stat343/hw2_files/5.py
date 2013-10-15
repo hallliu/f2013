873a597c93a72578b935a6f6031a82245e2122b2
Hall Liu <hallliu000@gmail.com>
2013-10-14 19:40:11 -0500

    s2

--- stat343/hw2_files/5.py
+++ stat343/hw2_files/5.py
@@ -24,5 +24,25 @@ beta1_se = np.sqrt(beta_cov[0,0])
 
 tdist = scipy.stats.t(6)
 t_crit_val = tdist.ppf(0.975)
-import ipdb;ipdb.set_trace()
 print 'CI:({0}, {1})'.format(beta[0] - beta1_se*t_crit_val, beta[0] + beta1_se*t_crit_val)
+
+R = np.matrix("1 1")
+xtxinv = linalg.inv(X.T * X)
+s_1= 1 / ((R * xtxinv * R.T)[0,0])
+
+rhb = beta.sum()
+sigmahat = np.sqrt(sigmasq)
+f_dist = scipy.stats.f(1,6)
+f_crit = f_dist.ppf(0.95)
+half_width = sigmahat * np.sqrt(f_crit / s_1)
+print "CI for b_1+b_2: ({0}, {1})".format(rhb - half_width, rhb + half_width)
+
+tval = (beta[1,0] - 3)/np.sqrt(beta_cov[1,1])
+p_val = 1 - tdist.cdf(tval)
+print tval, p_val
+
+R1 = np.matrix("1 -1")
+s_2 = 1 / ((R1 * xtxinv * R1.T)[0,0])
+rhb1 = (R1 * beta)[0,0]
+f_stat = rhb1 * rhb1 * s_2 / sigmahat
+print f_stat, 1 - f_dist.cdf(f_stat)
