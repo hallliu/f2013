e3625e73daa1b95190214420478039db12eb18c9
Hall Liu <hallliu000@gmail.com>
2013-12-08 17:24:57 -0600

    gsdl written

---
+++ stat309/hw5_files/gsdl.pyx
@@ -0,0 +1,50 @@
+import numpy as np
+cimport numpy as np
+import scipy.sparse as sp
+
+'''
+Gauss-Seidel, solves Ax=b. Works on csr matrices. Assume that b is a 
+plain 1d array, not sparse.
+'''
+def gsdl(A, np.ndarray[np.float64_t, ndim=1] b):
+    cdef np.ndarray[np.float64_t, ndim=1] x, old_x
+    x = np.random.randn(b.shape[0])
+    old_x = x.copy()
+
+    cdef int iter_counter = 0
+    cdef int i
+    cdef double A_diag
+    
+    # Objects for storing the data from A
+    cdef np.ndarray[np.int32_t, ndim=1] indptrs = A.indptrs
+    cdef np.ndarray[np.int32_t, ndim=1] indices = A.indices
+    cdef np.ndarray[np.float64_t, ndim=1] data = A.data
+
+    cdef np.ndarray[np.int32_t, ndim=1] row_indices
+    cdef np.ndarray[np.float64_t, ndim=1] row_data
+    cdef int indctr, j
+
+    while True:
+        for i in range(x.shape[0]):
+            row_indices = indices[indptrs[i]:indptrs[i+1]]
+            row_data = data[indptrs[i]:indptrs[i+1]]
+            indctr = 0
+
+            x[i] = b[i]
+            for j in row_indices:
+                # Only do something if we're not on the diagonal.
+                # Else, store it so we divide out by it later.
+                if j != i:
+                    x[i] -= row_data[indctr] * x[j]
+                else:
+                    A_diag = row_data[indctr]
+                indctr += 1
+            x[i] /= A_diag
+        
+        iter_counter += 1
+        if np.allclose(x, old_x):
+            break
+
+        old_x = x.copy()
+
+    return (x, iter_counter)
---
+++ stat309/hw5_files/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy as np
+
+gsdl_source = ['gsdl.pyx']
+
+setup(
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [Extension("gsdl", gsdl_sourcefiles, extra_compile_args=['-O3', '-std=gnu99'])]
+    include_dirs=[np.get_include()]
+)
