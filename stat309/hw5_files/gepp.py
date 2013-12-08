aed89c650d2bb2dc617535c77afb42ff28f9acbf
Hall Liu <hallliu000@gmail.com>
2013-12-08 02:52:20 -0600

    gepp dense done

--- stat309/hw5_files/gepp.py
+++ stat309/hw5_files/gepp.py
@@ -8,13 +8,36 @@ def gepp(a):
     l_orig = np.eye(a.shape[0], dtype=a.dtype)
     u_orig = a.A
 
+    permut = np.arange(a.shape[0], dtype='int32')
+
     for i in range(a.shape[0]):
         l = l_orig[i:, i:]
         u = u_orig[i:, i:]
 
-        l[1:, 0] = -u[1:, 0] / u[0, 0]
+        # Find max leading entry
+        maxind = np.argmax(np.abs(u[:, 0]))
+        # Swap them in the thing we're using to keep track of permutations
+        i1 = np.where(permut == maxind + i)
+        i2 = np.where(permut == i)
+        temp = permut[i1]
+        permut[i1] = permut[i2]
+        permut[i2] = temp
+
+        # Actually swap the rows
+        temp = u[0, :].copy()
+        u[0, :] = u[maxind, :]
+        u[maxind, :] = temp
+
+        # Swap the rows on the L matrix too
+        temp = l_orig[i, :i].copy()
+        l_orig[i, :i] = l_orig[maxind + i, :i]
+        l_orig[maxind + i, :i] = temp
+
+        # Do Gaussian elimination
+        l[1:, 0] = u[1:, 0] / u[0, 0]
         u[1:, 0] = 0
-        for j in range(1, u.shape[0]):
-            u[j, 1:] = u[j, 1:] + l[j, 0] * u[0, 1:]
+        for j in range(1, u.shape[1]):
+            u[1:, j] = u[1:, j] - l[1:, 0] * u[0, j]
+
     
-    return (l_orig, u_orig)
+    return (l_orig, u_orig, permut)
