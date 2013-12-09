094d51509cc4e29178d5a459f77c4d7acf248f13
Hall Liu <hallliu000@gmail.com>
2013-12-08 23:25:25 -0600

    processing shit

---
+++ stat309/hw5_files/process.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def runtimes_shape(density, results):
+    sizes = sorted(results.keys())
+    gepp_runtimes = list(map(lambda x: results[x][density][1]['gepp'], sizes))
+    sdsc_runtimes = list(map(lambda x: results[x][density][1]['sdsc'], sizes))
+    chol_runtimes = list(map(lambda x: results[x][density][1]['chol'], sizes))
+    gsdl_runtimes = list(map(lambda x: results[x][density][1]['gsdl'], sizes))
+
+    plt.figure()
+    plt.scatter(sizes, gepp_runtimes, marker='.')
+    plt.scatter(sizes, sdsc_runtimes, marker='o')
+    plt.scatter(sizes, chol_runtimes, marker='*')
+    plt.scatter(sizes, gsdl_runtimes, marker='+')
+    plt.yscale('log')
+    plt.show()
+
+def runtimes_density(shape, results):
+    ds = sorted(results[shape].keys())
+    sdsc_runtimes = list(map(lambda x: results[shape][x][1]['sdsc'], ds))
+    gsdl_runtimes = list(map(lambda x: results[shape][x][1]['gsdl'], ds))
+    gepp_runtimes = list(map(lambda x: results[shape][x][1]['gepp'], ds))
+    chol_runtimes = list(map(lambda x: results[shape][x][1]['chol'], ds))
+
+    plt.figure()
+    plt.scatter(ds, sdsc_runtimes, marker='o')
+    plt.scatter(ds, gsdl_runtimes, marker='+')
+    plt.scatter(ds, gepp_runtimes, marker='.')
+    plt.scatter(ds, chol_runtimes, marker='*')
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.show()
+
+def iterations_density(shape, results):
+    ds = sorted(results[shape].keys())
+    sdsc_iters = list(map(lambda x: results[shape][x][0]['sdsc'][1], ds))
+    gsdl_iters = list(map(lambda x: results[shape][x][0]['gsdl'][1], ds))
+
+    plt.figure()
+    plt.scatter(ds, sdsc_iters, marker='o')
+    plt.scatter(ds, gsdl_iters, marker='+')
+    plt.xscale('log')
+    plt.show()
+
+def iterations_shape(density, results):
+    sizes = sorted(results.keys())
+    sdsc_iters = list(map(lambda x: results[x][density][0]['sdsc'][1], sizes))
+    gsdl_iters = list(map(lambda x: results[x][density][0]['gsdl'][1], sizes))
+
+    plt.figure()
+    plt.scatter(sizes, sdsc_iters, marker='o')
+    plt.scatter(sizes, gsdl_iters, marker='+')
+    plt.show()
+
+def accuracy_shape(density, results):
+    sizes = sorted(results.keys())
+    gepp_accuracy = list(map(lambda x: results[x][density][0]['gepp'], sizes))
+    chol_accuracy = list(map(lambda x: results[x][density][0]['chol'], sizes))
+
+    plt.figure()
+    plt.scatter(sizes, gepp_accuracy, marker='.')
+    plt.scatter(sizes, chol_accuracy, marker='*')
+    plt.ylim(0, max(max(gepp_accuracy), max(chol_accuracy)))
+    plt.show()
+
+def accuracy_density(shape, results):
+    ds = sorted(results[shape].keys())
+    gepp_accuracy = list(map(lambda x: results[shape][x][0]['gepp'], ds))
+    chol_accuracy = list(map(lambda x: results[shape][x][0]['chol'], ds))
+
+    plt.figure()
+    plt.scatter(ds, gepp_accuracy, marker='.')
+    plt.scatter(ds, chol_accuracy, marker='*')
+    plt.ylim(0, max(max(gepp_accuracy), max(chol_accuracy)))
+    plt.xscale('log')
+    plt.show()
+
+
