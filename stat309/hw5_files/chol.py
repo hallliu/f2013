503e7352b8d060110ef2884ac6fcd840ee395e86
Hall Liu <hallliu000@gmail.com>
2013-12-08 21:34:56 -0600

    densified chol and gepp

--- stat309/hw5_files/chol.py
+++ stat309/hw5_files/chol.py
@@ -5,12 +5,10 @@ import scipy.sparse as sp
 Sparse Cholesky factorization (on lil matrices)
 '''
 def chol(a):
-    f = sp.lil_matrix(a.shape, dtype=a.dtype)
-    a_copy = sp.lil_matrix(a).copy()
+    f = np.zeros(a.shape, dtype=a.dtype)
+    a_copy = a.A
     for i in range(a.shape[0]):
         f[i:, i] = a_copy[i:, i] / np.sqrt(a_copy[i, i])
         a_copy[i+1:, i+1:] -= f[i+1:, i] * f[i+1:, i].T
 
     return f
-
-
--- stat309/hw5_files/gepp.py
+++ stat309/hw5_files/gepp.py
@@ -6,16 +6,15 @@ Takes a sparse matrix A(lil) and returns its LU factorization (both lil)
 '''
 def gepp(a):
     n = a.shape[0]
-    l = sp.identity(n, dtype=a.dtype, format='lil')
-    u = sp.lil_matrix(a).copy()
+    l = np.identity(n, dtype=a.dtype)
+    u = a.A
 
     permut = np.arange(a.shape[0], dtype='int32')
 
     for i in range(a.shape[0] - 1):
 
-        print(i)
         # Find max leading entry
-        maxind = np.argmax(np.abs(u[i:, i]).A)
+        maxind = np.argmax(np.abs(u[i:, i]))
 
         # Swap them in the thing we're using to keep track of permutations
         i1 = np.where(permut == maxind + i)
@@ -24,13 +23,11 @@ def gepp(a):
         permut[i1] = permut[i2]
         permut[i2] = temp
 
-        print('swapping rows')
         # Actually swap the rows
         temp = u[i, :]
         u[i, :] = u[maxind + i, :]
         u[maxind + i, :] = temp
 
-        print('swapping rows in L')
         # Swap the rows on the L matrix too
         if i != 0:
             temp = l[i, :i]
@@ -38,10 +35,9 @@ def gepp(a):
             l[maxind + i, :i] = temp
 
         # Do Gaussian elimination
-        print('doing GE')
         l[i+1:, i] = u[i+1:, i] / u[i, i]
         u[i+1:, i] = 0
         for j in range(i+1, u.shape[1]):
-            u[i+1:, j] = u[i+1:, j] - l[i+1:, i] * u[i, j]
-
+            u[i+1:, j] -= l[i+1:, i] * u[i, j]
+        
     return (l, u, permut)
